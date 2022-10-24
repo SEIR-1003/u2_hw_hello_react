ef2ce89216a58a56381bd0bf184484a026f1f172
Andrew <zhengandrew35@gmail.com>
2022-10-24 18:27:16 -0400

    bonus done

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import Hello from './Hello';
 
+
+
 const names = ['Eddie Van Halen', 'Jimi Hendrix', 'Wes Montgomery', 'Frank Zappa', 'John Mayer', 'Charizard', 'Blastoise', 'Venasaur']
 
 
